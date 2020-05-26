708ad6064241ac97ae4b1f467d3ed3d9df2d59e9
CodingSeo <seojingyo@hotmail.com>
2020-05-26 14:22:03 +0900

    ## client vue update

--- server/app/Category.php
+++ server/app/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
-    protected $fillable=['trade_info','book_info'];
+    //protected $fillable=['trade_info','book_info'];
     public function posts(){
         return $this->hasMany(Post::class);
     }

88f515913a88a6ad2f87aa7b1e2f762ea455bab0
CodingSeo <seojingyo@hotmail.com>
2020-06-03 19:36:49 +0900

    DO IT in HOME This is From ServicesProvider.php

--- server/app/Category.php
+++ server/app/Category.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    
+
     public $timestamps = false;
     public function posts(){
         return $this->hasMany(Post::class);
     }
+
 }

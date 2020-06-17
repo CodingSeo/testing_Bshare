3eccb076aa140556dc5717cfce4fd638e002576d
CodingSeo <seojingyo@hotmail.com>
2020-06-17 18:14:36 +0900

    ### this is ejrnaejwrnjalwe

--- server/app/Repositories/interfaces/PostRepository.php
+++ server/app/Repositories/interfaces/PostRepository.php
@@ -8,13 +8,6 @@ use App\DTO\PostDTO;
 
 interface PostRepository
 {
-    public function getOne(int $id): PostDTO;
-    public function findAll(): array;
-    public function updateByDTO(PostDTO $post): PostDTO;
-    public function updateByContent(array $post): PostDTO;
-    public function delete(PostDTO $content): bool;
-    public function save($content): PostDTO;
-
     public function getContent(PostDTO $post): ContentDTO;
     public function getComments(PostDTO $post): array;
     public function saveContent(int $post_id, array $content) : ContentDTO;
